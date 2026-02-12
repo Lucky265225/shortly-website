6e21c9541d6dc2749ef7a4a84063a6105eaea9ef
atiht dangpradub <Student@Train11-1-PC320.control.ad.sit.kmutt.ac.th>
2026-02-12 13:25:21 +0700

    start shortly-website

---
+++ src/components/Navbar.vue
@@ -0,0 +1,29 @@
+<script setup>
+
+</script>
+
+<template>
+    <!-- nav container -->
+    <nav>
+        <!-- flex container for all item -->
+         <div>
+            <!-- flex container for logo/menu -->
+             <div>
+                <!-- logo -->
+                 Shortly
+                 <img src="" alt="">
+                 <!-- left menu -->
+                  <div>
+                    <a href="">Features</a>
+                    <a href="">Pricing</a>
+                    <a href="">Resources</a>
+                  </div>
+             </div>
+         </div>
+    </nav>
+
+</template>
+
+<style scoped>
+
+</style>
